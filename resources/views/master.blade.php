cb0c1426a7c89da29511ca77575fda1fcff8c104
Jaden M. Lemmon <jmlemmo1@ip-10-16-1-39.ec2.internal>
2016-07-07 16:41:52 -0500

    added favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="News IO">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <title>SnugFeed</title>
     <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
     <link rel="stylesheet" href="/css/app.css">
