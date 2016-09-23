933f53d384998cd81a59112c3d6e735ad3c64f3a
jlemm45 <jadenlemmon@gmail.com>
2016-09-22 20:28:57 -0500

    deploy script change

--- hooks/post-deploy.sh
+++ hooks/post-deploy.sh
@@ -5,6 +5,7 @@ php /var/www/snug/artisan optimize
 php /var/www/snug/artisan view:clear
 php /var/www/snug/artisan cache:clear
 
+chown -R ubuntu:www-data /var/www/snug
 chmod -R 777 /var/www/snug/storage
 chmod -R 777 /var/www/snug/bootstrap
 
