6ee41c9a49d95bd5d0f314444896205c836c07e8
Jaden Lemmon <jaden@jadenlemmon.com>
2022-04-12 10:03:39 -0500

    bootstrap

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // default user
+        \App\Models\User::factory(1)->create(['email' => 'demo@snugfeed.com']);
     }
 }
